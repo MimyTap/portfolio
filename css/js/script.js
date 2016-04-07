f759f2028d8aafa4c1be8190fcc39f5aba6623e6
MimyTap <emilie.saby@outlook.fr>
2016-04-07 10:12:21 +0200

    modif competences

---
+++ css/js/script.js
@@ -0,0 +1,7 @@
+var $popup1 = document.getElementById('popup1');
+
+$popup1.addEventListener('click', function(popup1) { // L'argument « e » va récupérer une référence vers l'objet « Event »
+
+    alert("Niveau "); // Ceci affiche le type de l'événement (click, mouseover, etc.)
+
+});
